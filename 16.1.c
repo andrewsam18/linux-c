4a213066045badf38413fec07e3891e635114a02
andrewsam18 <andrewsam18@gmail.com>
2023-08-02 11:36:26 +0530

    16.1 Signed-off-by: andrewsam18 <andrewsam18@gmail.com>

--- 16.1.c
+++ 16.1.c
@@ -2,7 +2,7 @@
  int main (){
   int num1, num2, num3;
   puts("enter 3 numbers");
-  scanf("%i%i",&num1,num2,num3);
+  scanf("%i%i%i",&num1,&num2,&num3);
   if(num1>num2){
    if(num1>num3){
       printf("%i greater than %i and %i\n",num1,num2,num3);

73ddc2229a8ab491b89b6e98cf9f1d7130604fdf
andrewsam18 <andrewsam18@gmail.com>
2023-08-26 06:54:34 +0530

    8.cpp
    Signed-off-by: andrewsam18 <andrewsam18@gmail.com>

---
+++ cpp/8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class aclass{
+    public:
+    void printhello(){
+cout <<"hello";
+    }
+};
+
+int main(){
+     aclass object;
+     object.printhello();
+      return 0;
+}

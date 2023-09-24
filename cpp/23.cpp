c46cdcdf77d478a662622852c37a9e837c3261df
andrewsam18 <andrewsam18@gmail.com>
2023-09-24 14:44:56 +0530

    ipjkijk

---
+++ cpp/23.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void passbyvalue(int a){
+    a=45;
+}
+void passbyreference(int* a){
+*a=45;
+}
+int main(){
+int x=10;
+passbyvalue(x);
+cout<<x<<endl;
+int x1=10;
+passbyreference(&x1);
+cout<<x1<<endl;
+    return 0;
+}

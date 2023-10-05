a24efa591d4f6a4dc927d31137ba20b2e69864a1
andrewsam18 <andrewsam18@gmail.com>
2023-10-06 01:01:23 +0530

    1

---
+++ cpp/36.2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+template<class T>
+class number{
+
+
+
+
+6.07
+{
+    return a+b;
+}};
+//we use this types to give any datatypes like int, double,float
+int main(){
+    int a=5;
+ double  b=7.5;
+    cout<<add(a,b)<<endl;
+        return 0;
+}
+

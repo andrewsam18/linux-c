51f6000ba8d18c7edf9ae4a233265f9148c5854a
andrewsam18 <andrewsam18@gmail.com>
2024-03-27 06:17:51 +0530

    modified:   bash/8operators1.sh

--- bash/8operators1.sh
+++ bash/8operators1.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 
+echo "Arithmatic Operators"
 
 read -p "enter n1:" n1
 read -p "enter n2:" n2
 
-echo "addition"$((n1+n2))
+#echo "addition":$((n1+n2))
+
+#echo "subtraction:"$((n1-n2))
+
+#echo "multiplcation":$((n1*n2))
+
+#echo "division:"$((n1/n2))
+
+#echo "modulus:"$((n1%n2))
+
+#echo "increment:"$((n1++))
+
+#echo "increment2:"$((++n1))
+#echo "decrement:"$((--n2))
+#echo "decrement2:"$((n2--))
+
+echo "Relational Operators"
+if [ $n1 -eq $n2 -o $n1 -gt 1 ]
+then
+
+echo "n1 = n2"
+
+fi

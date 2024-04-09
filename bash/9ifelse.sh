1d963c2dddb6687fcb1c06d52f0137844e488d6a
andrewsam18 <andrewsam18@gmail.com>
2024-04-09 18:15:23 +0530

    modified:   bash/9ifelse.sh

--- bash/9ifelse.sh
+++ bash/9ifelse.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-a=11
- if [ $a -eq 10 ]
+#a=11
+ #if [ $a -eq 10 ]
+ #then
+#echo "a is equal to 10"
+ #else
+ #echo "a is not equal to 10"
+ #fi
+ read -p "enter your age" age
+ #if [ $age -ge 18 ]
+  if [ $age -gt 18 ]
  then
-echo "a is equal to 10"
+ echo "your are eligible to case vote"
+ elif [ $age -eq 18 ]
+ then
+ echo "please apply for voter id card"
  else
- echo "a is not equal to 10"
+ echo "your are  not eligible to case vote"
  fi
- read age

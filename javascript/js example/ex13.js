6daabcd252a6c64f56e5d43502674f26c31a84ae
andrew samuel <andrewsam18@gmail.com>
2025-02-24 00:55:51 +0530

    h

--- javascript/js example/ex13.js
+++ javascript/js example/ex13.js
@@ -1,1 +1,11 @@
-let eyeicon =
+let eyeicon =document.getElementById("eyeicon");
+let password= document.getElementById("password");
+eyeicon.onclick=function(){
+    if(passwordInput.type==="password"){
+        passwordInput.type="text";
+    eyeicon.src ="image/eye-open.png";
+}else{
+    passwordInput.type="password";
+    eyeicon.src="imgage/eye-close";
+}
+};
